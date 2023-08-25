cb08917d1c45aa348ae779714e539bcb5f732935
neeraj-gs <gsneeraj05@gmail.com>
2023-08-25 09:49:58 +0530

    Created and added a logo for our editor using Canva

--- group-code/src/components/Home.jsx
+++ group-code/src/components/Home.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='homePageWrapper'>
+      <div className='formWrapper'>
+        <form action="">
+          
+        </form>
+      </div>
+    </div>
   )
 }
 
