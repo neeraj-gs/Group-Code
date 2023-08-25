2dfa128dbd1afd50f06acacce7d745c57334ec9a
neeraj-gs <gsneeraj05@gmail.com>
2023-08-25 10:00:30 +0530

    Created a Basic Skeleton of Home Page

--- group-code/src/components/Home.jsx
+++ group-code/src/components/Home.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
+import Logo from '../assets/GroupCode_Logo.png'
 
 const Home = () => {
   return (
     <div className='homePageWrapper'>
       <div className='formWrapper'>
-        <form action="">
-          
-        </form>
+        <img src={Logo} alt='Group Code Logo'/>
+        <h4 className='mainLabel'>Paste Invitation ROOM Id</h4>
+        <div className="inputGroup">
+          <input type="text" className='inputBox' placeholder='ROOM Id'/>
+          <input type="text" className='inputBox' placeholder='Username'/>
+          <button className="btn joinBtn">Join</button>
+          <span className="craeteInfo">Dont have Invite Code? Create &nbsp; <a href="" className='createNewBtn'>New Room</a></span>
+        </div>
+        
       </div>
     </div>
   )
