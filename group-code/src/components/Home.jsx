a80ecd01c87f66e9e6e82bd5e3ffce31b7d9e83f
neeraj-gs <gsneeraj05@gmail.com>
2023-08-25 11:46:11 +0530

    Added Styles to the Home Page

--- group-code/src/components/Home.jsx
+++ group-code/src/components/Home.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
-import Logo from '../assets/GroupCode_Logo.png'
+import Logo from '../assets/Logo_Small.png'
+import '../styles/Home.css'
 
 const Home = () => {
   return (
     <div className='homePageWrapper'>
       <div className='formWrapper'>
-        <img src={Logo} alt='Group Code Logo'/>
-        <h4 className='mainLabel'>Paste Invitation ROOM Id</h4>
+        <img className='logo'  src={Logo} alt='Group Code Logo'/>
+        <h4 className='mainLabel'>Paste Invitation ROOM ID</h4>
         <div className="inputGroup">
-          <input type="text" className='inputBox' placeholder='ROOM Id'/>
+          <input type="text" className='inputBox' placeholder='ROOM ID'/>
           <input type="text" className='inputBox' placeholder='Username'/>
           <button className="btn joinBtn">Join</button>
-          <span className="craeteInfo">Dont have Invite Code? Create &nbsp; <a href="" className='createNewBtn'>New Room</a></span>
+          <span className="createInfo">Dont have Invite Code? Create &nbsp; <a href="" className='createNewBtn'>New Room</a></span>
         </div>
         
       </div>
