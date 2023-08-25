db5ae7140011527eb15801cac84aea905a6da12e
neeraj-gs <gsneeraj05@gmail.com>
2023-08-25 19:00:57 +0530

    Added code to craete a random string from uuid pacakge

--- group-code/src/components/Home.jsx
+++ group-code/src/components/Home.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
 import Logo from '../assets/Logo_Small.png'
 import '../styles/Home.css'
+import {v4 as uuidV4} from 'uuid'
 
 const Home = () => {
+
+  const createNewRoom = (e) =>{
+    e.preventDefault();
+    const id = uuidV4();
+    console.log(id);
+  }
+
   return (
     <div className='homePageWrapper'>
       <div className='formWrapper'>
@@ -12,7 +20,7 @@ const Home = () => {
           <input type="text" className='inputBox' placeholder='ROOM ID'/>
           <input type="text" className='inputBox' placeholder='Username'/>
           <button className="btn joinBtn">Join</button>
-          <span className="createInfo">Dont have Invite Code? Create &nbsp; <a href="" className='createNewBtn'>New Room</a></span>
+          <span className="createInfo">Dont have Invite Code? Create &nbsp; <a onClick={createNewRoom} href="" className='createNewBtn'>New Room</a></span>
         </div>
         
       </div>
