c3c345b776035a687fdb4d6782875466decc991c
neeraj-gs <gsneeraj05@gmail.com>
2023-08-25 09:22:30 +0530

    Added Rotues in App for rendering Diffrent Pages on Diffrent Routes/URL

--- group-code/src/App.jsx
+++ group-code/src/App.jsx
@@ -1,14 +1,20 @@
-import { useState } from 'react'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import './App.css'
+import Home from './components/Home'
+import EditorPage from './components/EditorPage'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div>
-      
-    </div>
-    
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/editor/:roomId" element={<EditorPage />}></Route>
+        </Routes>
+      </BrowserRouter>
+  
+    </>    
   )
 }
 
