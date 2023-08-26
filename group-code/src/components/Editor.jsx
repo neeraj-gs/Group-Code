746536340dcee6fa74807ef43fb92bccfe0bcb40
neeraj-gs <gsneeraj05@gmail.com>
2023-08-26 20:14:59 +0530

    Added codemirror editor and an instance of it onto the screen

--- group-code/src/components/Editor.jsx
+++ group-code/src/components/Editor.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
+import React, { useEffect ,useRef} from 'react'
+import Codemirror from 'codemirror'
+import 'codemirror/mode/javascript/javascript'
+import 'codemirror/theme/dracula.css'
+import 'codemirror/addon/edit/closetag'
+import 'codemirror/addon/edit/closebrackets'
+import 'codemirror/lib/codemirror.css'
 
 const Editor = () => {
-  return (
-    <div>
+  const textareaRef = useRef(null);
+  const codeMirrorInstanceRef = useRef(null);
+
+    useEffect(()=>{ //so that it is rendered only once
+      if(!codeMirrorInstanceRef.current){
+        codeMirrorInstanceRef.current = Codemirror.fromTextArea(
+        textareaRef.current,
+        { //this creates a mirror ont the text area and adds its editor
+          mode: {name:'javascript',json:true}, //optiosn to configure editor , to enable mode we need to import some jsvascript
+          theme:'dracula', //to enale it we need to import CSS
+          autoCloseTags:true,
+          autoCloseBrackets:true,
+          lineNumbers:true,
+      }
+     );
+    }
+    
+  },[]);
         
-    </div>
+    
+  return (
+    <textarea ref={textareaRef} id="realtimeEditor"></textarea>
   )
 }
 
