52830599db6521518ab3ac5c2b301ce0e1f1fde2
neeraj-gs <gsneeraj05@gmail.com>
2023-08-26 18:30:50 +0530

    Created a State for Clients as of now it si hardcoded and we map each client into a new client component

--- group-code/src/components/EditorPage.jsx
+++ group-code/src/components/EditorPage.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Logo from '../assets/Filled_Logo.png'
 
 const EditorPage = () => {
+  const [clients,setClients] = useState([{
+    socketId:1,
+    username:'Neeraj'
+  },{
+    socketId:2,
+    username:'Boss'
+  }]);
   return (
-    <div>EditorPage</div>
+    <div className='mainWrap'>
+      <div className="aside">
+        <div className='asideInner'>
+          <div className="logo">
+            <img src={Logo} alt="logo" className='logoImage' />
+          </div>
+          <h3>Coders Connected</h3>
+          <div className="clientsList">
+            {clients.map(c=><Client key={client.socketId} username={clients.username}/>)}
+
+          </div>
+        </div>
+      </div>
+      <div className="editorWrap">Editor Goes Here</div>
+    </div>
   )
 }
 
