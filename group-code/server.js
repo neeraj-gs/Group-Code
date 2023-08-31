d39c7585d9237114bfd3965f407eae99499dae58
neeraj-gs <gsneeraj05@gmail.com>
2023-08-31 12:20:21 +0530

    Added a fucntion of socket on what to do after a coneciton to the socket instance

--- group-code/server.js
+++ group-code/server.js
@@ -6,6 +6,24 @@ const server = http.createServer(app); //We create a server and then pass the ex
 const io =  new Server(server);//server class instance is being created
 
 
+io.on('connection',(socket)=>{
+    console.log('Scocket Connected',socket.id);
+})//triggered as sson as some socekt is coonnected to thte server
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 const PORT = process.env.PORT||5000
 server.listen (PORT,()=>{
     console.log(`Listening on Port: ${PORT}`)
