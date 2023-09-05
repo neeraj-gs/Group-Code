4f9275ff6ce3533c3fb5a849a3922decd5fe70e0
neeraj-gs <gsneeraj05@gmail.com>
2023-09-05 21:10:39 +0530

    Created a Development Setup for the Socket.io and server side components

--- group-code/server.js
+++ group-code/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
-const {Server} =require("socket.io") //it is a class provider by socket
 const app = express();
+const {Server} =require("socket.io") //it is a class provider by socket
+
 const http = require('http'); //It is a Http module inbuilt in nodejs
 const server = http.createServer(app); //We create a server and then pass the express server obejct app
 const io =  new Server(server);//server class instance is being created
